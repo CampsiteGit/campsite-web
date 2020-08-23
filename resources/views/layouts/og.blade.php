ac840ac76f6ec2306d757f397e8ecd22e51945f7
Iskawo <44570552+Iskawo@users.noreply.github.com>
2020-08-23 02:08:27 +0100

    some additional stuff to open graph

--- resources/views/layouts/og.blade.php
+++ resources/views/layouts/og.blade.php
@@ -1,11 +1,12 @@
 <!-- Websites ETC -->
 <meta name="og:site_name" content="The Campsite"/>
-<meta name="og:title" content="Join The Campsite community today!"/>
+<meta name="og:title" content="The Campsite - Join today"/>
 <meta name="og:description" content="We are an ever-growing community on Discord that aims to provide a warm welcome to all new members joining us!"/>
 <meta name="og:url" content="https://discord.com/invite/campsite/"/>
-<meta name="og:image" content="{{ asset('img/thecampsitegif-128.gif') }}"/>
-<meta name="og:border-color" content="#FFFF77"/>
-<meta name="og:color" content="#FFFF77"/>
+<meta name="apple-mobile-web-app-title" content="The Campsite"/>
+<link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/png">
+<meta property="og:site_name" content="The Campsite"/>
+<link rel="mask-icon" sizes="any" href="{{ asset('img/thecampsitetext.png') }}" color="#FFFF77">
 
 <!-- Twitter -->
 <meta name="description" content="We are an ever-growing community on Discord that aims to provide a warm welcome to all new members joining us!" />
